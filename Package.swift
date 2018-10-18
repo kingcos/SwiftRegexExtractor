706976bd8c5ea3f13e397df5bdec6395fc3483ab
kingcos <2821836721v@gmail.com>
2018-10-18 21:35:34 +0800

    Seperate RegexExtractorKit

--- Package.swift
+++ Package.swift
@@ -15,11 +15,19 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftRegexExtractor",
-            dependencies: []),
+            dependencies: [
+                "RegexExtractorKit"
+            ]),
+        .target(
+            name: "RegexExtractorKit",
+            dependencies: [
+                "Spectre",
+                "PathKit"
+            ]),
         .testTarget(
             name: "SwiftRegexExtractorTests",
             dependencies: [
-                "SwiftRegexExtractor",
+                "RegexExtractorKit",
                 "Spectre",
                 "PathKit"
             ]),
